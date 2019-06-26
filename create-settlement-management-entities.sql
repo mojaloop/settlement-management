d46d8fbe262bdaab6fbc8c6adf3a433b3edc77bc
Aarón Reynoza <ldaaron38@gmail.com>
2019-06-26 10:46:13 -0600

    add .sql file for participantSettlementBankAccountNumber table and its dockerfile

---
+++ create-settlement-management-entities.sql
@@ -0,0 +1,15 @@
+/*
+ * (C)2019 ModusBox Inc.
+ * =====================
+ * Project: Casablanca
+ * Original Author: Aarón Reynoza
+ * Description: This script creates database entities required by the Casablanca Settlement Management
+ */
+
+ CREATE TABLE IF NOT EXISTS participantSettlementBankAccountNumber (
+    participantSettlementBankAccountNumberId INT UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT 'Surrogate primary key',
+    participantId INT UNSIGNED NOT NULL COMMENT 'The participant holding this account',
+    accountCountry VARCHAR(3) NOT NULL COMMENT 'The code that represents the country',
+    accountNumber VARCHAR(256) NOT NULL COMMENT 'The externally encrypted account number',
+    createdDate DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT 'System dateTime stamp pertaining to the inserted record'
+);
