1327328a2eaf73d0c0cab996dabe0d50dcdbe832
Aarón Reynoza <ldaaron38@gmail.com>
2019-07-02 06:09:33 -0600

    add participantCurrencyId and foreing key to table

--- create-settlement-management-entities.sql
+++ create-settlement-management-entities.sql
@@ -6,10 +6,11 @@
  * Description: This script creates database entities required by the Casablanca Settlement Management
  */
 
- CREATE TABLE IF NOT EXISTS participantSettlementBankAccountNumber (
+CREATE TABLE IF NOT EXISTS participantSettlementBankAccountNumber (
     participantSettlementBankAccountNumberId INT UNSIGNED AUTO_INCREMENT PRIMARY KEY COMMENT 'Surrogate primary key',
-    participantId INT UNSIGNED NOT NULL COMMENT 'The participant holding this account',
+    participantCurrencyId INT UNSIGNED NOT NULL COMMENT 'The settlement currency corresponding to this bank account',
     accountCountry VARCHAR(3) NOT NULL COMMENT 'The code that represents the country',
     accountNumber VARCHAR(256) NOT NULL COMMENT 'The externally encrypted account number',
-    createdDate DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT 'System dateTime stamp pertaining to the inserted record'
+    createdDate DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT 'System dateTime stamp pertaining to the inserted record',
+    CONSTRAINT participantsettlementbankaccountnumber_participantcurrency_foreign FOREIGN KEY (participantCurrencyId) REFERENCES participantCurrency (participantCurrencyId)
 );
